7677139372012ecf936759c3612afa4ef7e009c4
Jan Kaiser <jan.kaiser@telefonica.com>
2019-07-30 13:58:57 +0200

    cleanup and eslint

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+// eslint-disable-next-line no-unused-vars
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <div>
         Hello World!
