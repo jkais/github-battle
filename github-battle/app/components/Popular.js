098313e231cb2d427d58119f6270869f21e619ec
Jan Kaiser <jan.kaiser@telefonica.com>
2019-07-31 12:09:44 +0200

    add state to Popular nav

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,13 +1,35 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+    
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage (lang) {
+    this.setState(
+      {
+        selectedLanguage: lang
+      }
+    )
+  }
+
   render () {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
       <ul className='flex-center'>
-        {languages.map((language) => (
+        {this.languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>{language}</button>
+            <button 
+              className='btn-clear nav-link'
+              style={ language === this.state.selectedLanguage ? { color: 'green' } : null }
+              onClick={() => this.updateLanguage(language)}>{language}</button>
           </li>
         ))}
       </ul>
