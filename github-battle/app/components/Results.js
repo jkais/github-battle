9d6a440bc037a7f3fcc695b23044c871a9f97c51
Jan Kaiser <jan.kaiser@telefonica.com>
2019-08-02 18:49:51 +0200

    calculate battle results

--- github-battle/app/components/Results.js
+++ github-battle/app/components/Results.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+
+  componentDidMount () {
+    const { playerOne, playerTwo } = this.props
+    battle([playerOne, playerTwo])
+      .then((players) => console.log("Players: ", players))
+  }
+
   render () {
     return (
       <div>
--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -1,3 +1,69 @@
+// if rate limiting becomes a problem, add oauth token stuff here...
+const id = 'MY_ID'
+const sec = 'MY_SECRET'
+const params = `?params=client_id=${id}&client_secret=${sec}`
+
+function getErrorMessage (message, username) {
+  if (message === 'Not found') {
+    return `${username} doesn't exist`
+  } else {
+    return message
+  }
+}
+
+function getProfile (username) {
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMessage(profile.message, username))
+      }
+
+      return profile
+    })
+}
+
+function getRepos (username) {
+  return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMessage(repos.message, username))
+      }
+
+      return repos
+    })
+}
+
+function getStarCount(repos) {
+  return repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0)
+}
+
+function calculatedScore (followers, repos) {
+  return (followers * 3) + getStarCount(repos)
+}
+
+function getUserData (player) {
+  return Promise.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(([ profile, repos ]) => ({
+    profile,
+    score: calculatedScore(profile.followers, repos)
+  }))
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle(players) {
+  return Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1])
+  ]).then((players) => sortPlayers(players))
+}
+
 export function fetchPopularRepos(language) {
   const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
