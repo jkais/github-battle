0905d59cf1baf3e316b052f4340d69bab509dc39
Jan Kaiser <jan.kaiser@telefonica.com>
2019-08-06 13:36:19 +0200

    make a reusable Card function

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -39,28 +39,32 @@ function ReposGrid ({ repos }) {
 
         return (
           <li key={html_url} className="card bg-light">
-            <h4 className="header-lg center-text">#{index + 1}</h4>
-            <img className="avatar" src={avatar_url}/>
-            <h2 className="center-text"><a className="link" href={html_url}>{name}</a></h2>
-
-            <ul className="card-list">
-              <li>
-                <FaUser color='rgb(255, 191, 161)' size={22}/>
-                <a href={`https://github.com/${login}`}>{login}</a>
-              </li>
-              <li>
-                <FaStar color='rgb(255, 215, 0)' size={22}/>
-                {stargazers_count.toLocaleString()} stars
-              </li>
-              <li>
-                <FaCodeBranch color='rgb(129, 195, 255)' size={22}/>
-                {forks.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22}/>
-                {open_issues.toLocaleString()} open issues
-              </li>
-            </ul>
+            <Card
+              header={`#${index + 1}`}
+              subheader={`Score: ${winner.score}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className="card-list">
+                <li>
+                  <FaUser color='rgb(255, 191, 161)' size={22}/>
+                  <a href={`https://github.com/${login}`}>{login}</a>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22}/>
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 255)' size={22}/>
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22}/>
+                  {open_issues.toLocaleString()} open issues
+                </li>
+              </ul>
+            </Card>
           </li>
         )
       })}
