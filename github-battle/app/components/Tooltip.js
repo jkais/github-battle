4117fa1e697edd5837e740233847f3763433b7eb
Jan Kaiser <jan.kaiser@telefonica.com>
2019-08-06 20:40:34 +0200

    Tooltip now with Higher Order Component

--- github-battle/app/components/Tooltip.js
+++ github-battle/app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { timingSafeEqual } from 'crypto';
+import WithHover from './WithHover'
 
 const styles = {
   container: {
@@ -24,44 +24,18 @@ const styles = {
   }
 }
 
-export default class Tooltip extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      hovering: false
-    }
-
-    this.mouseOver = this.mouseOver.bind(this)
-    this.mouseOut = this.mouseOut.bind(this)
-  }
-
-  mouseOver () {
-    this.setState({
-      hovering: true
-    })
-  }
-
-  mouseOut () {
-    this.setState({
-      hovering: false
-    })
-  }
-
-  render () {
-    const { children, text } = this.props
-    const { hovering } = this.state
-    return (
-      <div
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-        style={styles.container}
-      >
-        {hovering && (
-          <div style={styles.tooltip}>{text}</div>
-        )}
-        {children}
-      </div>
-    )
-  }
+function Tooltip ({ text, children, hovering }) {
+  return (
+    <div style={styles.container}>
+      {hovering && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  )
 }
+
+Tooltip.propTypes = {
+  text: PropTypes.string.isRequired,
+  hovering: PropTypes.bool.isRequired
+}
+
+export default WithHover(Tooltip)
