e7118e587a0e32811dbe33f036a47770fb157a82
Jan Kaiser <kaiser.jan@gmail.com>
2019-08-11 21:17:28 +0200

    fix 404 problems on netlify

--- github-battle/webpack.config.js
+++ github-battle/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebkacPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
@@ -18,7 +19,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyWebkacPlugin([
+      {from: '_redirects'}
+    ])
   ],
   devServer: {
     historyApiFallback: true
